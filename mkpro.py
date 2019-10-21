946c08dc60280791867e6c4b42b26444f60c8142
Kevin Sidhu <sidhu.kevin@gmail.com>
2019-10-21 16:59:19 -0400

    add create venv

--- mkpro.py
+++ mkpro.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 
 def read_settings():
     with open("settings.json", "r") as settings_file:
@@ -20,13 +21,17 @@ def create_project_folder(folder_path, project_name):
 
 # TODO(1): create a venv
 def create_venv():
+    subprocess.call(["py", "-m", "venv", "venv2"])
     pass
+
 # TODO(2): git init
 # TODO(3): .gitignore
 # TODO(4): git push
 # TODO(5): open up code in WSL
 
 if __name__ == "__main__":
-    settings = read_settings()
-    project_name = input("## Enter the project name: ")
-    create_project_folder(settings["RootFolderPath"], project_name)
+    # settings = read_settings()
+    # project_name = input("## Enter the project name: ")
+    # create_project_folder(settings["RootFolderPath"], project_name)
+    create_venv()
+    
